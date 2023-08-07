005f28764359895cff9c2b7fbf17ffd3538f5988
Pietro <pietro.sammarco@googlemail.com>
2023-08-07 18:24:13 +0100

    wip, broken

--- landscape/clients/conf/focal/init/entrypoint.sh
+++ landscape/clients/conf/focal/init/entrypoint.sh
@@ -4,13 +4,14 @@
 # ----------------------------------------------------------
 # Start landscape-client daemon  
 # ----------------------------------------------------------
-/etc/init.d/landscape-client start
+#/etc/init.d/landscape-client start
 
 # ----------------------------------------------------------
 # Setup client configuration 
 # ----------------------------------------------------------
-landscape-config --computer-title "focal" --account-name standalone \
---url https://"$FRONTEND"/message-system --ping-url http://"FRONTEND"/ping
+#landscape-config --computer-title "focal" --account-name standalone \
+#--url https://"$FRONTEND"/message-system --ping-url http://"FRONTEND"/ping \
+#--script-users=ALL --ok-no-register --silent
 
 
 # Keep contain alive
